8641e0385679fc93033e2faea05d06b467c22b3f
JeremT8 <jeremytilletpro@gmail.com>
2022-01-06 11:24:33 +0100

    exercice complete

--- MyApp/src/app/services/todo.service.ts
+++ MyApp/src/app/services/todo.service.ts
@@ -7,8 +7,8 @@ import { Todo } from '../models/todo.model';
 export class TodoService {
 
   taskList: Todo[] = [
-    new Todo({taskName: "Faire le menage", done: false}),
-    new Todo({taskName: "Apprendre l'espagnol", done: false})
+    new Todo({taskName: "Faire le menage", done: false, id: 1}),
+    new Todo({taskName: "Apprendre l'espagnol", done: false, id: 2}),
   ];
   
   constructor() {
@@ -23,5 +23,12 @@ export class TodoService {
    addTask(data: Todo): void {
      this.taskList.push(data);
    }
+
+   deleteTask(id: number | undefined): void {
+    const index = this.taskList.findIndex(item => item.id == id);
+    this.taskList.splice(index, 1);
+  }
+
+  
    
 }
